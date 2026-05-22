f17e14b8fcfd7ab51e6c3ccb0333463306837527
Oliver Tran <137974300+olivertransf@users.noreply.github.com>
2026-05-22 14:39:57 -0700

    Initial Commit

---
+++ Soundfolio/SoundfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundfolioApp.swift
+//  Soundfolio
+//
+//  Created by Oliver Tran on 5/22/26.
+//
+
+import SwiftUI
+
+@main
+struct SoundfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
